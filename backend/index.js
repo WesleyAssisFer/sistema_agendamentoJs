4d91d21ce682f0d0ebbb89ccdf25a760a546fbf3
Wesley <wesleyassis2002@gmail.com>
2026-06-13 11:20:04 -0300

    refactor: Melhoria visual nas rotas, somente o index, define o nome da rotaprincipal

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ const app = express();
 const salaRoutes = require("./routes/salaRoutes");
 const professorRouter = require("./routes/professorRoutes");
 
-app.use(salaRoutes);
-app.use(professorRouter);
+app.use("/salas", salaRoutes);
+app.use("/professores", professorRouter);
 
 app.listen(3000, () => {
     console.log("Servidor rodando!");
